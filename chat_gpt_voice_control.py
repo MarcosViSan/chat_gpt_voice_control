3b9ee9b8220c88318d84340e45c24bdd3e878a15
Marcos Santos <marksleek555@gmail.com>
2023-11-12 19:13:09 -0300

    Saida de audio e atualização da api do OpenAI

--- chat_gpt_voice_control.py
+++ chat_gpt_voice_control.py
@@ -1,11 +1,17 @@
 import openai 
 import requests
 import speech_recognition as sr
+import pyttsx3
+engine = pyttsx3.init()
 
 r = sr.Recognizer()
 
+client = openai.Client(api_key='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')
+
+engine.say("Pergunte Me Qualquer coisa.")
+engine.runAndWait()
+
 with sr.Microphone() as source:
-    print("Diga algo...")
     audio = r.listen(source)
 
 try:
@@ -17,26 +23,26 @@ except sr.RequestError as e:
     print("Erro ao solicitar o serviço de reconhecimento de fala; {0}".format(e))
 
 def txt_chatgpt (text):
-  openai.api_key = "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"
+  openai.api_key = "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"
 
-  url = "https://api.openai.com/v1/engines/text-davinci-002/completions"
+#   url = "https://api.openai.com/v1/completions"
   prompt= text
 
-  params = {
-      "prompt": prompt,
-      "max_tokens": 500,
-      "n": 1,
-      "stop": None,
-      "temperature": 0.7
-    }
-
-  response = requests.post(url, headers={"Authorization": f"Bearer {openai.api_key}"}, json=params)
+  response = client.chat.completions.create(
+    model="gpt-3.5-turbo-1106",
+    response_format={ "type": "json_object" },
+    messages=[
+      {"role": "system", "content": "You are a helpful assistant designed to output short phrases in brazilian Portuguese"},
+      {"role": "user", "content": prompt}
+    ]
+  )
 
   if response.status_code != 200:
       print(f"Erro ao gerar resposta: {response.text}")
   else:
       data = response.json()
-      text = data["choices"][0]["text"]
-      print(text)
+      text = data["choices"][0]["message"]
+      engine.say(text)
+      engine.runAndWait()
 
 txt_chatgpt(text)
